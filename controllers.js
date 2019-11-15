9bdb4b49248f3f7d4c1921d514a6c7015fd21664
r-o-b <rbehrenswi@zoho.com>
2019-11-15 15:40:35 -0600

    first commit

---
+++ controllers.js
@@ -0,0 +1,167 @@
+﻿
+app.controller("IndexController", function($scope, $location, $log) {
+    $log.debug("IndexController start");
+    
+    $scope.onHomeScreen = function() {
+        $log.debug("onHomeScreen() start with $location.path() == " + $location.path() );
+        var path = $location.path();
+        return (path == "/" || path == "/index.htm");
+    }
+
+});
+
+
+app.controller("HomeController", function($scope, $location, $log, AnimalFactory) {
+    $scope.animals = AnimalFactory.animals;
+    $scope.currentAnimal = $scope.animals[0]; //"Froggy"
+    $log.debug("HomeController start");
+    
+    $scope.start = function() {
+        $location.hash("0");
+    }
+
+});
+
+
+app.controller("MainController", function($scope, $location, $timeout, $log, AnimalFactory) {
+    $scope.settings = {};
+    $scope.settings.numActions = 3;
+    $scope.actSelected = "";
+    $log.debug("MainController() start");
+    $scope.addGrassCss = false;
+    $timeout( function() {
+        $scope.addGrassCss = true;
+    }, 100)
+    
+    var turn, autoplayTimeout;
+
+    var availActions = AnimalFactory.actions;
+    
+    var animals = AnimalFactory.animals;
+    
+    $scope.animals = animals;
+    $scope.radioModelSpeed = '3000';
+    
+    initGame();
+    
+    
+    $scope.next = function(eventObj) {
+        //$log.debug("next() argument is: ");
+        //$log.debug(eventObj);
+        $log.debug("next() start");
+        if (eventObj) {
+            $log.debug("next() eventObj.target.value == " + eventObj.target.value);
+            $scope.actSelected = eventObj.target.value;
+            $scope.inTransition = "fromNew";
+        } else {
+            $scope.inTransition = "fromRepeat";
+        }
+        recordPreviousTurn();
+        turn++;
+        $timeout(startNextTurn, 500); 
+    }
+    
+    $scope.reset = function() {
+        $log.debug("reset() start");
+        initGame();
+        $location.hash("0");
+    }
+    
+    $scope.check = function() {
+        $log.debug("check() start");
+        $scope.checkingAnswer = true;
+        $scope.pause();
+    }
+    
+    $scope.play = function() {
+        $log.debug("play() start");
+        $scope.autoplay = true;
+        autoPlayLoop();
+    }
+    
+    $scope.pause = function() {
+        $log.debug("pause() start");
+        if (autoplayTimeout) {
+            $timeout.cancel(autoplayTimeout);
+        }
+        $scope.autoplay = false;
+    }
+    
+    $scope.numActionsChanged = function() {
+        $log.debug("numActionsChanged() start");
+        $scope.actions = AnimalFactory.getActions($scope.currentAnimal.primary, $scope.settings.numActions);
+    }
+    
+    function autoPlayLoop() {
+        $log.debug("autoPlayLoop() start");
+        $scope.next();
+        if ($scope.autoplay == true) {
+            autoplayTimeout = $timeout(autoPlayLoop, $scope.radioModelSpeed);
+        }
+    }
+    
+    function startNextTurn() {
+        $log.debug("startNextTurn() start");
+
+        //get ready for the next turn! //debug +1
+        $scope.currentAnimal = AnimalFactory.getRandomAnimal( $scope.answers );
+        if ( isNewAnimal($scope.currentAnimal.name) ) {
+            $scope.pause();
+            $scope.actions = AnimalFactory.getActions($scope.currentAnimal.primary, $scope.settings.numActions);
+        }
+        
+        //reset state
+        $scope.checkingAnswer = false;
+        $location.hash(turn);
+        
+        $timeout(function() {
+            $log.debug("startNextTurn() setting $scope.actSelected to ''");
+            $scope.actSelected = "";
+            $scope.inTransition = false;
+            setFocus();
+        }, 100); //adding just a little delay to give image time to switch
+    }
+    
+    function recordPreviousTurn() {
+        $log.debug("recordPreviousTurn() start with $scope.actSelected == " + $scope.actSelected);
+        var currentAnimal = $scope.currentAnimal.name
+        var currentAnimalImage = AnimalFactory.getImageFor(currentAnimal);
+        if ( isNewAnimal(currentAnimal) ) {
+            $scope.answers[currentAnimal] = $scope.actSelected;
+        }
+        $scope.turns.push({
+            "animal": currentAnimal,
+            "action": $scope.actSelected,
+            "image": currentAnimalImage
+        });
+        $log.debug("recordPreviousTurn() end");
+    }
+    
+    function isNewAnimal(animalName) {
+        return !$scope.answers[animalName];
+    }
+    
+    function setFocus() {
+        //because getting autofocus attribute to re-apply is even hacky-er
+        $log.debug("setFocus() start");
+        if ($scope.answers[$scope.currentAnimal.name]) {
+            document.getElementById("nextButton").focus();
+            $log.debug("setFocus() put #nextButton in focus");
+        } else {
+            //$log.debug("document.getElementById('action0').innerText == " + document.getElementById("action0").innerText );
+            document.getElementById("action0").focus();
+            $log.debug("setFocus() put #action0 in focus");
+        }
+    }
+    
+    function initGame() {
+        turn = 0;
+        $scope.currentAnimal = animals[0]; //"Froggy"
+        $scope.actions = ["Hop", "Spin", "Flap"];    
+        $scope.turns = [];
+        $scope.answers = {};
+        $scope.actSelected = "";
+        $scope.recapToggle = false; /* close recap pane if it's open */
+    }
+    
+});
---
+++ froggycise.js
@@ -0,0 +1,143 @@
+﻿
+var app = angular.module('froggyApp', ['ui.bootstrap', 'ngRoute']);
+
+app.config(function($locationProvider, $routeProvider, $logProvider) {
+    $locationProvider.html5Mode(true);
+    
+    $logProvider.debugEnabled(false);
+    
+    $routeProvider
+        .when('/',
+            {
+                controller: 'MainController',
+                templateUrl: 'homeView.htm'
+            })
+        .when('/index.htm',
+            {
+                controller: 'MainController',
+                templateUrl: 'homeView.htm'
+            })
+        .when('/play',
+            {
+                controller: 'MainController',
+                templateUrl: 'playView.htm',
+                reloadOnSearch: false
+            })
+        .otherwise({ redirectTo: '/' });
+        
+});
+
+app.factory('AnimalFactory', function($log) {
+    $log.debug("AnimalFactory start");
+    var factory = {};
+    factory.animals = [
+    {"name":"Froggy", "image": "Bagou_Froggy_cartoon_04.svg", "primary":"Hop"},
+    {"name":"Donkey", "image": "Donkey_cartoon_04.svg", "primary":"Stomp"},
+    {"name":"Bull", "image": "Bull_cartoon_04.svg", "primary":"Squat"},
+    {"name":"Goat", "image": "Goat_cartoon_04.svg", "primary":"Tip-toes"},
+    {"name":"Sheep", "image": "Sheep_cartoon_04.svg", "primary":"Touch toes"},
+    {"name":"Rooster", "image": "Rooster_cartoon_04.svg", "primary":"Stretch"},
+    {"name":"Goose", "image": "Goose_cartoon_04.svg", "primary":"Jumping-jack"},
+    {"name":"Pig", "image": "Pig_cartoon_04.svg", "primary":"Squat"},
+    {"name":"Chicks", "image": "Hen_and_chicks_cartoon_04.svg", "primary":"Spin"},
+    {"name":"Cow", "image": "Cow_cartoon_04.svg", "primary":"Kick"},
+    {"name":"Chicken", "image": "Chicken_cartoon_04.svg", "primary":"Flap"}
+    ];
+    factory.actions = [
+        "Hop",
+        "Spin",
+        "Run",
+        "Kick",
+        "Stretch",
+        "Flap",
+        "Stomp",
+        "Squat",
+        "Tip-toes",
+        "Touch toes",
+        "Jumping-jack"
+    ];
+    /* parameter:
+        animal (required) -- string -- name of animal
+    returns:
+        string -- file name of svg image file for that animal */
+    factory.getImageFor = function(animal) {
+        $log.debug("AnimalFactory getImageFor() start with animal == " + animal);
+        var animalImage;
+        for (var i=0; i<factory.animals.length; i++) {
+            if (factory.animals[i].name == animal) {
+                animalImage = factory.animals[i].image;
+            }
+        }
+        $log.debug("AnimalFactory getImageFor() is returning: " + animalImage);
+        return animalImage;
+    }
+    /* parameter:
+        animalName (required) -- string of name of animal (e.g. "Froggy", "Cow")
+    returns:
+        object -- animal object that has requested name property (along with image src path, primary action, etc.) */
+    factory.getAnimalObjectByName = function (animalName) {
+        var animalMatch;
+        for (var i = 0, animalsLen = factory.animals.length; i<animalsLen; i++) {
+            if (factory.animals[i].name == animalName) {
+                animalMatch = factory.animals[i];
+            }
+        }
+        return animalMatch;
+    }
+    /* parameter:
+        animalsLikelyArray (optional) 
+            -- array of strings -- names of animals that will be more likely to come up (at least 50% of the time)
+        OR  -- object with properties that are the names of animals...
+     returns:
+        object -- reference to 1 animal object (which includes name, image src, etc) */
+    factory.getRandomAnimal = function (animalsLikelyArray) {
+        $log.debug("AnimalFactory getRandomAnimal() start");
+        //$log.debug(animalsLikelyArray);
+        var animalStr,
+            animalObj,
+            tempArray = [];
+            
+        /* if parameter isn't an array, then make it one */
+        if ( animalsLikelyArray && !angular.isArray(animalsLikelyArray) ) {
+            angular.forEach(animalsLikelyArray, function(value, key) {
+                tempArray.push(key);
+            });
+            animalsLikelyArray = tempArray;
+        }
+        
+        if ( animalsLikelyArray && (Math.random() > .5) ) {
+            animalStr = animalsLikelyArray[Math.floor(Math.random() * animalsLikelyArray.length)];
+            animalObj = factory.getAnimalObjectByName(animalStr);
+        } else {
+            animalObj = factory.animals[Math.floor(Math.random() * factory.animals.length)];
+        }
+        return animalObj;
+    }
+    /* paraemters:
+        none
+    returns:
+        string -- 1 action randomly chosen from all in AnimalFactory */
+    factory.getRandomAction = function () {
+        $log.debug("AnimalFactory getRandomAction() start");
+        return factory.actions[Math.floor(Math.random() * factory.actions.length)];
+    }
+    /* parameters:
+        include (optional? should be, but haven't tested that way) -- one action that should be included 1st in return array
+        howMany (required, though could make optional with default like 3) -- number of actions to return in array
+    returns:
+        array -- of actions (strings), with 'include' one on top */
+    factory.getActions = function (include, howMany) {
+        $log.debug("AnimalFactory getActions() start");
+        var actArray = [include];
+        var tempAct;
+        while (actArray.length < howMany) {
+            tempAct = factory.getRandomAction();
+            if (actArray.toString().indexOf(tempAct) == -1) {
+                actArray.push(tempAct);
+            }
+        }
+        $log.debug("AnimalFactory getActions() end");
+        return actArray;
+    }
+    return factory;
+})
